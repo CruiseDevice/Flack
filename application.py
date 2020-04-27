404913d44bb56f98962efbc9da8cfa79a0811bc0
CruiseDevice <meakashchavan@gmail.com>
2020-04-27 21:01:37 +0530

    Multiple changes
    - User can join and leave room
    - show notification when user joins and leaves room
    - User Bootstrap for UI

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
+
 from forms import LoginForm
 
 from credentials import SECRET_KEY
@@ -11,44 +12,55 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
 socketio = SocketIO(app)
 
+room_list = []
+user_list = []
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	form = LoginForm()
-	if form.validate_on_submit():
-		session['name'] = form.name.data
-		session['room'] = form.room.data
-		return redirect(url_for('chat'))
-	elif request.method == 'GET':
-		form.name.data = session.get('name', '')
-		form.room.data = session.get('room', '')
-	return render_template('index.html', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        session['room'] = form.room.data
+        room_list.append(session['room'])
+        return redirect(url_for('chat'))
+    elif request.method == 'GET':
+        form.name.data = session.get('name', '')
+        form.room.data = session.get('room', '')
+    return render_template('index.html', form=form, room_list=room_list)
 
 
 @app.route('/chat')
 def chat():
-	name = session.get('name', '')
-	room = session.get('room', '')
-	if name == '' or room == '':
-		return redirect(url_for('index'))
-	return render_template('chat.html', name=name, room=room)
+    name = session.get('name', '')
+    room = session.get('room', '')
+
+    if name == '' or room == '':
+        return redirect(url_for('index'))
+    return render_template('chat.html', name=name, room=room)
 
 
 @socketio.on('joined', namespace='/chat')
 def joined(message):
-	pass
-	room = session.get('room')
-	join_room(room)
-	emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+    room = session.get('room')
+    join_room(room)
+    emit('status', {
+        'msg': session.get('name') + ' has entered the room.'
+    }, room=room)
 
 
 @socketio.on('text', namespace='/chat')
 def text(message):
     room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    emit('message', {
+        'msg': session.get('name') + ':' + message['msg']
+    }, room=room)
 
 
 @socketio.on('left', namespace='/chat')
 def left(message):
-	room = session.get('room')
-	leave_room(room)
-	emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
+    room = session.get('room')
+    leave_room(room)
+    emit('status', {
+        'msg': session.get('name') + ' has left the room.'
+    }, room=room)
